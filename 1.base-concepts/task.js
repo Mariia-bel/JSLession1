b735de41a181f486b82e3cdd8346acbd0a0509d9
Мария <airbelka@bk.ru>
2022-09-11 22:50:51 +0300

    Add code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
+"use strict"
+
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let ansver = [];
+  if (a==0) return "ошибка";
+  let d = b * b - 4 * a *c;
+  let x1, x2;
+  if (d > 0) {
+      x1 = (-b + Math.sqrt(d)) / (2 * a);
+      x2 = (-b - Math.sqrt(d)) / (2 * a);
+      ansver = [x1, x2]
+      return ansver;    
+  } else if (d == 0) {
+      x1 = -b /(2 * a);
+      ansver = [x1]
+      return ansver;
+  } else if (d < 0) {
+    ansver = [d];
+      return ansver;
+  }
+  
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
