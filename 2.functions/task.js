e7680a228d2abb0bb53b88be7b9b1e7dda1408bf
Мария <airbelka@bk.ru>
2022-09-16 19:48:50 +0300

    Add code to functions/task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,46 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = - Infinity;
+  let sum = 0;
+  
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] < min)
+    min = arr[i];
+    if (arr[i] > max)
+    max = arr[i];
+    sum += arr[i]
+  }
 
+  let avg = +(sum / arr.length).toFixed(2);
+  
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  max = func(arrOfArr[0]);
+  for (let i = 1; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+
+  return Math.abs(getArrayParams(arr).max - getArrayParams(arr).min);
 }
