db6df62f67f05a42eb44866518e7aa639f5e7c4a
Мария <airbelka@bk.ru>
2022-09-16 14:09:41 +0300

    Fix code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,25 +2,24 @@
 
 
 function solveEquation(a, b, c) {
-  let ansver = [];
-  if (a==0) return "ошибка";
-  let d = b * b - 4 * a *c;
+  let ansver;
+  if (a===0) {
+    return "ошибка"
+  }
+  const d = b * b - 4 * a *c;
   let x1, x2;
+  
   if (d > 0) {
       x1 = (-b + Math.sqrt(d)) / (2 * a);
       x2 = (-b - Math.sqrt(d)) / (2 * a);
-      ansver = [x1, x2]
-      return ansver;    
-  } else if (d == 0) {
+      ansver = [x1, x2];   
+  } else if (d === 0) {
       x1 = -b /(2 * a);
-      ansver = [x1]
-      return ansver;
-  } else if (d < 0) {
-    ansver = [d];
-      return ansver;
+      ansver = [x1];
+  } else{
+      ansver = []
   }
   
+return ansver;
 }
-
-
 
