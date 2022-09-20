0a47bf50ce7fa905c0a81438a1b99203c50b037e
Мария <airbelka@bk.ru>
2022-09-20 23:45:40 +0300

    Add code to arrays/task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
-function compareArrays(arr1, arr2) {
-  let result;
+"use strict"
 
-  // Ваш код
+function compareArrays(arr1, arr2) {
+
+  let result = arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
