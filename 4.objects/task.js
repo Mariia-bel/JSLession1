a79add5b4a1f8b35c04a08fa2263238cfc96cfcc
Мария <airbelka@bk.ru>
2022-09-27 14:28:18 +0300

    Add code

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+new Student ("Ivan", "male", 25)
+new Student ("Ira", "female", 23)
+new Student ("Pavel", "male", 22)
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks]
+  } else {
+    this.marks.push(...marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  return sum / this.marks.length;
+
+};
+
+Student.prototype.exclude = function (reason) {
+
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
